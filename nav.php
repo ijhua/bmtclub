0463a924dacda8b8e44760032b2a38fdbf182a6b
ijhua <hua.isabelle@gmail.com>
2018-07-03 13:45:55 -0700

    fix the navbar issues

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
     <a href="index.php#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-question-circle"></i> ABOUT</a>
     <a href="index.php#join" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-users"></i> JOIN</a>
     <a href="index.php#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-address-card"></i> CONTACT</a>
-    <div class="dropdown">
+    <div class="dropdown w3-hide-small">
       <button class="dropbtn">MORE <i class="fas fa-caret-down"></i>
     </button>
       <div class="dropdown-content">
@@ -20,10 +20,10 @@
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-    <a href="#announcements" class="w3-bar-item w3-button" onclick="toggleFunction()">ANNOUNCEMENTS</a>
-    <a href="#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
-    <a href="#join" class="w3-bar-item w3-button" onclick="toggleFunction()">JOIN</a>
-    <a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+    <a href="index.php#announcements" class="w3-bar-item w3-button" onclick="toggleFunction()">ANNOUNCEMENTS</a>
+    <a href="index.php#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+    <a href="index.php#join" class="w3-bar-item w3-button" onclick="toggleFunction()">JOIN</a>
+    <a href="index.php#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
     <a href="officers.php" class="w3-bar-item w3-button" onclick="toggleFunction()">MEET THE OFFICERS</a>
   </div>
 </div>
